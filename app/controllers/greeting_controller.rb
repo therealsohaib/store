f87536f70628e41b5cef8821c62af2f534113205
Sohaib Hassan <sohaib@elgrocer.com>
2025-08-29 14:51:58 +0500

    Greeting controller setup

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,4 @@
+class GreetingController < ApplicationController
+  def hello
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,38 @@
 class ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product, status: :ok
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
