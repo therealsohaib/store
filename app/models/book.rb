204d61b2255be705590b1df9be6e64eaf1f1f0ad
Sohaib Hassan <sohaib@elgrocer.com>
2025-09-01 17:05:28 +0500

    Callbacks added in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
+  before_update :before_logs
+  after_update :after_logs
 
+  private
+  def before_logs
+    puts "Book updating"
+  end
+
+  def after_logs
+    puts "Book updated"
+  end
 end
